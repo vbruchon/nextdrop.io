49d76c4f8e9aa441566194d3e979a3470d6d5a7a
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-06-12 18:00:40 +0200

    remove base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -6,8 +6,6 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth.ts";
 
 export const authClient = createAuthClient({
-  // TODO : Change this to the production URL
-  baseURL: "http://localhost:3000",
   plugins: [magicLinkClient(), inferAdditionalFields<typeof auth>()],
 });
 
